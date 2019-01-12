fa04835d2c3390b9345fde01eaf1b854d0131d16
sumanp9 <11.sumanpradhan@gmail.com>
2019-01-12 12:16:27 -0600

    added reading of a file and outputting it out to the textArea.

--- src/sample/Editor.java
+++ src/sample/Editor.java
@@ -1,15 +1,21 @@
 package sample;
 
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-import java.io.File;
+import java.io.*;
 
 public class Editor {
 
     public TextArea txtArea;
     public Button  btnBold, btnItalic, btnUnderline;
     public MenuBar menuBar;
+    private Stage stage;
+    public AnchorPane anchorPane;
+
+
     public MenuItem open = new MenuItem("Open File"),
             save =  new MenuItem("Save File") ,
             close = new MenuItem("Exit");
@@ -22,20 +28,59 @@ public class Editor {
         createMenus();
 
         open.setOnAction(e -> {
-            openFiles();
+            try {
+                openFiles();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                System.out.println("Cannot opeen the file that u were trying to.");
+            }
+        });
+
+        close.setOnAction(event -> {
+            closeWindow();
         });
 
     }
 
-    private void openFiles() {
+    private void closeWindow() {
+        stage = (Stage)anchorPane.getScene().getWindow();
+        stage.close();
+    }
+
+    private void openFiles() throws IOException {
 
         FileChooser fileChooser =  new FileChooser();
         fileChooser.setTitle("Open File");
+
+        stage = (Stage)anchorPane.getScene().getWindow();
+
         fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Text Files", "*.txt","*.rtf")
+                new FileChooser.ExtensionFilter("Text Files  *.txt, *.rtf", "*.txt","*.rtf")
         );
+        File file  = fileChooser.showOpenDialog(stage);
+
+        readFile(file);
+
+
+        /*if (file != null){
+            stage.display(file);
+        }*/
         // needs work check previous projects .
 
+    }
+
+    private void readFile(File file) throws IOException {
+
+        BufferedReader buffer =  new BufferedReader(new FileReader(file));
+        txtArea.clear();
+        String string, lines = "";
+
+        while((string =  buffer.readLine())!=null){
+            //System.out.println(string);
+            lines+=string+"\n";
+        }
+
+        txtArea.setText(lines);
     }
 
     private void createMenus() {
